5b18e77defa9bbb31f5fe590623b56b9009baa8c
IlyuAfanasev <gecktor.86@mail.ru>
2023-07-03 22:12:53 +0400

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/h_m_2_11_mockito/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.h_m_2_11_mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
